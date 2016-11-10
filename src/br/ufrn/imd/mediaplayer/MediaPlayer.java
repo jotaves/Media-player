12ba912c0f237d7c9b07c2ad771e39352b9a89c0
Joao Victor Bezerra Barboza <jotave@A307-34L.imd.local>
2016-11-10 20:19:34 -0300

    Primeiro commit

---
+++ src/br/ufrn/imd/mediaplayer/MediaPlayer.java
@@ -0,0 +1,17 @@
+package br.ufrn.imd.mediaplayer;
+
+/**
+ *
+ * @author João Victor
+ * @author Pedro Arthur
+ */
+public class MediaPlayer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO
+    }
+    
+}
